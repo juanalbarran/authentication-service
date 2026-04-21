fc3d89b40f4471d5c24b3365f5eaf4f5c3a21c9a
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-21 18:05:53 +0200

    User, UserSession
    Added getters and setters

--- src/main/java/com/jarsolutions/authentication/domain/entity/User.java
+++ src/main/java/com/jarsolutions/authentication/domain/entity/User.java
@@ -9,9 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "user_account")
 public class User {
@@ -34,5 +32,37 @@ public class User {
   public User(String username, String password) {
     this.username = username;
     this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public List<UserSession> getUserSession() {
+    return userSession;
+  }
+
+  public void setUserSession(List<UserSession> userSession) {
+    this.userSession = userSession;
   }
 }
--- src/main/java/com/jarsolutions/authentication/domain/entity/UserSession.java
+++ src/main/java/com/jarsolutions/authentication/domain/entity/UserSession.java
@@ -10,9 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.Instant;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "user_session")
 public class UserSession {
@@ -40,6 +38,46 @@ public class UserSession {
     this.token = token;
     this.deviceInfo = deviceInfo;
     this.expiryDate = expiryDate;
+    this.user = user;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public String getDeviceInfo() {
+    return deviceInfo;
+  }
+
+  public void setDeviceInfo(String deviceInfo) {
+    this.deviceInfo = deviceInfo;
+  }
+
+  public Instant getExpiryDate() {
+    return expiryDate;
+  }
+
+  public void setExpiryDate(Instant expiryDate) {
+    this.expiryDate = expiryDate;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
     this.user = user;
   }
 }

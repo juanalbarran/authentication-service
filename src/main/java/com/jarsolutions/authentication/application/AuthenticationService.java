cc16108e28ac7e90f18dd077950ab266286c9ae7
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-21 18:02:35 +0200

    JwtService
    Created the JwtService added more functionality to AuthenticationService

--- src/main/java/com/jarsolutions/authentication/application/AuthenticationService.java
+++ src/main/java/com/jarsolutions/authentication/application/AuthenticationService.java
@@ -1,10 +1,15 @@
 package com.jarsolutions.authentication.application;
 
-import com.jarsolutions.authentication.domain.User;
-import com.jarsolutions.authentication.domain.UserRepository;
+import com.jarsolutions.authentication.domain.entity.User;
+import com.jarsolutions.authentication.domain.entity.UserSession;
+import com.jarsolutions.authentication.domain.exception.UserAlreadyExistsException;
+import com.jarsolutions.authentication.domain.repository.UserRepository;
+import com.jarsolutions.authentication.domain.repository.UserSessionRepository;
+import java.time.Instant;
 import lombok.Data;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Data
@@ -12,24 +17,62 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
   private final UserRepository userRepository;
+  private final UserSessionRepository userSessionRepository;
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
+  private final PasswordEncoder passwordEncoder;
 
   public AuthenticationService(
       UserRepository userRepository,
+      UserSessionRepository userSessionRepository,
       AuthenticationManager authenticationManager,
-      JwtService jwtService) {
+      JwtService jwtService,
+      PasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
     this.authenticationManager = authenticationManager;
     this.jwtService = jwtService;
+    this.passwordEncoder = passwordEncoder;
+    this.userSessionRepository = userSessionRepository;
   }
 
-  public record LoginResult(String accessToken, String RefreshToken, User user) {}
+  public record UserResult(Long id, String username) {}
+  ;
+
+  public record LoginResult(String accessToken, String refreshToken, UserResult userResult) {}
+  ;
+
+  public record RegisterResult(String accessToken, String refreshToken, UserResult userResult) {}
   ;
 
   public LoginResult login(String username, String password, String deviceType) {
     authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 
     return null;
+  }
+
+  public RegisterResult register(String username, String password, String deviceInfo)
+      throws UserAlreadyExistsException {
+    if (userRepository.existsByUsername(username)) {
+      throw new UserAlreadyExistsException("User " + username + " already exists.");
+    }
+
+    String hashedPassword = passwordEncoder.encode(password);
+
+    User user = new User(username, hashedPassword);
+    User savedUser = userRepository.save(user);
+
+    String accessToken = jwtService.generateAccessToken(savedUser.getUsername());
+    String refreshToken = jwtService.generateRefreshToken(savedUser.getUsername());
+
+    Instant refreshTokenExpiration =
+        Instant.now().plusMillis(jwtService.getRefreshTokenExpiration());
+
+    UserSession userSession =
+        new UserSession(refreshToken, deviceInfo, refreshTokenExpiration, savedUser);
+    UserSession savedUserSession = userSessionRepository.save(userSession);
+
+    UserResult userResult = new UserResult(savedUser.getId(), savedUser.getUsername());
+
+    return new RegisterResult(accessToken, refreshToken, userResult);
   }
 }
--- src/main/java/com/jarsolutions/authentication/application/JwtService.java
+++ src/main/java/com/jarsolutions/authentication/application/JwtService.java
@@ -1,9 +1,44 @@
 package com.jarsolutions.authentication.application;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
   public JwtService() {}
+
+  private String secretKey;
+  private long accessTokenExpiration;
+  private long refreshTokenExpiration;
+
+  public String generateAccessToken(String username) {
+    return buildToken(username, accessTokenExpiration);
+  }
+
+  public String generateRefreshToken(String username) {
+    return buildToken(username, refreshTokenExpiration);
+  }
+
+  public long getRefreshTokenExpiration() {
+    return refreshTokenExpiration;
+  }
+
+  private String buildToken(String username, long expirationTime) {
+    return Jwts.builder()
+        .subject(username)
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + expirationTime))
+        .signWith(getSignInKey())
+        .compact();
+  }
+
+  private SecretKey getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
 }

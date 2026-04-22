ae5c4e165236ddb46ce16328e36b89d4465ea683
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-22 17:48:03 +0200

    AuthenticationService
    Finished the /register endpoint
    Added the @Transactional annotation to avoid failures in the methods

--- src/main/java/com/jarsolutions/authentication/application/AuthenticationService.java
+++ src/main/java/com/jarsolutions/authentication/application/AuthenticationService.java
@@ -1,19 +1,24 @@
 package com.jarsolutions.authentication.application;
 
+import com.jarsolutions.authentication.application.input.RegisterInput;
+import com.jarsolutions.authentication.application.output.LoginOutput;
+import com.jarsolutions.authentication.application.output.RegisterOutput;
+import com.jarsolutions.authentication.application.output.TokenOutput;
+import com.jarsolutions.authentication.application.output.UserOutput;
 import com.jarsolutions.authentication.domain.entity.User;
 import com.jarsolutions.authentication.domain.entity.UserSession;
 import com.jarsolutions.authentication.domain.exception.UserAlreadyExistsException;
 import com.jarsolutions.authentication.domain.repository.UserRepository;
 import com.jarsolutions.authentication.domain.repository.UserSessionRepository;
+import jakarta.transaction.Transactional;
 import java.time.Instant;
-import lombok.Data;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Data
 @Service
+@Transactional
 public class AuthenticationService {
 
   private final UserRepository userRepository;
@@ -21,6 +26,10 @@ public class AuthenticationService {
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
   private final PasswordEncoder passwordEncoder;
+
+  private record CreateUserInput(String username, String password) {}
+
+  private record CreateUserSessionInput(String refreshToken, String deviceInfo, User user) {}
 
   public AuthenticationService(
       UserRepository userRepository,
@@ -35,44 +44,53 @@ public class AuthenticationService {
     this.userSessionRepository = userSessionRepository;
   }
 
-  public record UserResult(Long id, String username) {}
-  ;
-
-  public record LoginResult(String accessToken, String refreshToken, UserResult userResult) {}
-  ;
-
-  public record RegisterResult(String accessToken, String refreshToken, UserResult userResult) {}
-  ;
-
-  public LoginResult login(String username, String password, String deviceType) {
+  public LoginOutput login(String username, String password, String deviceType) {
     authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 
     return null;
   }
 
-  public RegisterResult register(String username, String password, String deviceInfo)
-      throws UserAlreadyExistsException {
+  public RegisterOutput register(RegisterInput registerInput) throws UserAlreadyExistsException {
+
+    String username = registerInput.username();
+    String password = registerInput.password();
+
     if (userRepository.existsByUsername(username)) {
       throw new UserAlreadyExistsException("User " + username + " already exists.");
     }
 
-    String hashedPassword = passwordEncoder.encode(password);
+    User user = createUser(new CreateUserInput(username, password));
+    TokenOutput tokens = createTokens(username);
 
-    User user = new User(username, hashedPassword);
+    createUserSession(
+        new CreateUserSessionInput(tokens.refreshToken(), registerInput.deviceInfo(), user));
+    UserOutput userOutput = new UserOutput(user.getId(), username);
+
+    return new RegisterOutput(tokens, userOutput);
+  }
+
+  private User createUser(CreateUserInput createUserInput) {
+    String hashedPassword = passwordEncoder.encode(createUserInput.password());
+    User user = new User(createUserInput.username(), hashedPassword);
     User savedUser = userRepository.save(user);
+    return savedUser;
+  }
 
-    String accessToken = jwtService.generateAccessToken(savedUser.getUsername());
-    String refreshToken = jwtService.generateRefreshToken(savedUser.getUsername());
+  private TokenOutput createTokens(String username) {
+    String accessToken = jwtService.generateAccessToken(username);
+    String refreshToken = jwtService.generateRefreshToken(username);
+    return new TokenOutput(accessToken, refreshToken);
+  }
 
+  private void createUserSession(CreateUserSessionInput createUserSessionInput) {
     Instant refreshTokenExpiration =
         Instant.now().plusMillis(jwtService.getRefreshTokenExpiration());
-
     UserSession userSession =
-        new UserSession(refreshToken, deviceInfo, refreshTokenExpiration, savedUser);
-    UserSession savedUserSession = userSessionRepository.save(userSession);
-
-    UserResult userResult = new UserResult(savedUser.getId(), savedUser.getUsername());
-
-    return new RegisterResult(accessToken, refreshToken, userResult);
+        new UserSession(
+            createUserSessionInput.refreshToken(),
+            createUserSessionInput.deviceInfo(),
+            refreshTokenExpiration,
+            createUserSessionInput.user());
+    userSessionRepository.save(userSession);
   }
 }

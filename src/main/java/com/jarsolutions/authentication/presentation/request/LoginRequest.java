d97ab7f6f4089a3fdf64039cd751c22ce4fac78b
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-22 17:50:34 +0200

    Request
    All the request are now records

--- src/main/java/com/jarsolutions/authentication/presentation/request/LoginRequest.java
+++ src/main/java/com/jarsolutions/authentication/presentation/request/LoginRequest.java
@@ -1,17 +1,3 @@
 package com.jarsolutions.authentication.presentation.request;
 
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-
-  private String username;
-  private String password;
-  private String deviceType;
-
-  public LoginRequest(String username, String password, String deviceType) {
-    this.username = username;
-    this.password = password;
-    this.deviceType = deviceType;
-  }
-}
+public record LoginRequest(String username, String password, String deviceInfo) {}
--- src/main/java/com/jarsolutions/authentication/presentation/request/RegisterRequest.java
+++ src/main/java/com/jarsolutions/authentication/presentation/request/RegisterRequest.java
@@ -1,15 +1,3 @@
 package com.jarsolutions.authentication.presentation.request;
 
-import lombok.Data;
-
-@Data
-public class RegisterRequest {
-
-  private String username;
-  private String password;
-
-  public RegisterRequest(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
-}
+public record RegisterRequest(String username, String password, String deviceInfo) {}

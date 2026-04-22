4cd261becf5d9b65a9067e1deec1b47b317c3dc0
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-22 17:51:25 +0200

    AuthenticationController
    Finished the /register enpoint

--- src/main/java/com/jarsolutions/authentication/presentation/AuthenticationController.java
+++ src/main/java/com/jarsolutions/authentication/presentation/AuthenticationController.java
@@ -1,9 +1,14 @@
 package com.jarsolutions.authentication.presentation;
 
+import com.jarsolutions.authentication.application.AuthenticationService;
+import com.jarsolutions.authentication.application.input.RegisterInput;
+import com.jarsolutions.authentication.application.output.RegisterOutput;
 import com.jarsolutions.authentication.presentation.request.LoginRequest;
 import com.jarsolutions.authentication.presentation.request.RegisterRequest;
 import com.jarsolutions.authentication.presentation.response.AuthResponse;
 import com.jarsolutions.authentication.presentation.response.RegisterResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
 
+  private final AuthenticationService authenticationService;
+
+  public AuthenticationController(AuthenticationService authenticationService) {
+    this.authenticationService = authenticationService;
+  }
+
   @PostMapping("/login")
   public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
     return null;
   }
 
   @PostMapping("/register")
-  public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest RegisterRequest) {
-    return null;
+  public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
+    RegisterInput registerInput =
+        new RegisterInput(
+            registerRequest.username(), registerRequest.password(), registerRequest.deviceInfo());
+    RegisterOutput registerOutput = authenticationService.register(registerInput);
+    ResponseCookie refreshTokenCookie =
+        ResponseCookie.from("refresh_token", registerOutput.tokenOutput().refreshToken())
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(7 * 24 * 60 * 60)
+            .build();
+    RegisterResponse registerResponse =
+        new RegisterResponse(registerOutput.tokenOutput().accessToken());
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
+        .body(registerResponse);
   }
 }

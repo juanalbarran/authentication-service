f04ba2a53d5ffc02df6ba8f81e276e009512db59
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-30 10:10:57 +0200

    Refresh
    Added the method findByToken needed for the /refresh endpoint

--- src/main/java/com/jarsolutions/authentication/domain/repository/UserSessionRepository.java
+++ src/main/java/com/jarsolutions/authentication/domain/repository/UserSessionRepository.java
@@ -1,8 +1,11 @@
 package com.jarsolutions.authentication.domain.repository;
 
 import com.jarsolutions.authentication.domain.entity.UserSession;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserSessionRepository extends JpaRepository<UserSession, Long> {}
+public interface UserSessionRepository extends JpaRepository<UserSession, Long> {
+  Optional<UserSession> findByToken(String refreshToken);
+}
